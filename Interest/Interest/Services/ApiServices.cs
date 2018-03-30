3a553102e10f183005ce42040fae9967ce874536
MichaelChrystal <S00164997@mail.itsligo.ie>
2018-03-30 14:42:03 +0100

    Mobile added & RegisterPage Completed

---
+++ Interest/Interest/Services/ApiServices.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using Interest.Models;
+using Newtonsoft.Json;
+
+namespace Interest.Services
+{
+    public class ApiServices
+    {//reg user
+        public async Task<bool> RegisterAsync(string email, string password, string confirmPassword)
+        {
+            var client = new HttpClient();
+
+            var model = new RegisterBindingModel
+            {
+                Email = email,
+                Password = password,
+                ConfirmPassword = confirmPassword
+            };
+
+            var json = JsonConvert.SerializeObject(model);
+
+            HttpContent content = new StringContent(json);
+
+            var response = await client.PostAsync("http://localhost:63724/api/Account/Register",content);
+
+            return response.IsSuccessStatusCode;
+        }
+    }
+}
---
+++ Interest/Interest/ViewModels/RegisterViewModel.cs
@@ -0,0 +1,41 @@
+﻿using Interest.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+using Xamarin.Forms;
+
+namespace Interest.ViewModels
+{
+    public class RegisterViewModel
+    {
+        ApiServices _apiServices = new ApiServices();
+
+        public string Email { get; set; }
+
+        public string Password { get; set; }
+
+        public string ConfirmPassword { get; set; }
+
+        public ICommand RegisterCommand
+        {
+            get
+            {
+                string Message;
+                return new Command(async() =>
+                {
+                    var isSuccess = await _apiServices.RegisterAsync(Email, Password, ConfirmPassword);
+                    if (isSuccess)
+                          Message = "Registered Successfully";
+                    else
+                    {
+                        Message = "Retry later";
+                    }
+                });
+            }
+        }
+    }
+}
